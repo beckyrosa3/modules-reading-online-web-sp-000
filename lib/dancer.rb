c539e64ca9457cab7e1269f6be0449faf8b0962d
Becky Rosa <becky_m_rosa@yahoo.com>
2021-05-17 03:01:15 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,5 +1,5 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+#require_relative './class_methods_module.rb'
+#require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
@@ -12,5 +12,4 @@ class Dancer
     def initialize(name)
       @name = name
   end
-
 end
